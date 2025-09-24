4863c9db1d437c2c74ec6a771bb494b20b4e6787
pouriazizo-cmyk <pouriazizo@gmail.com>
2025-09-25 03:28:47 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 # Start script for background remover
 #!/bin/bash
-
-# فعال کردن پینگ خودکار در محیط production
-export ENVIRONMENT=production
-
-# شروع برنامه
-echo "🚀 Starting Background Remover App with Auto-Ping..."
 gunicorn --bind 0.0.0.0:$PORT --preload app:app
